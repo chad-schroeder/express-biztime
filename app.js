1cba5881c969acce83f90d876ff19d3c1151b822
Chad Schroeder <gmchad@gmail.com>
2018-12-13 10:53:31 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+/** BizTime express application. */
+
+
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+module.exports = app;
